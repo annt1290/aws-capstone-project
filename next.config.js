09bb2e1b56e598923851906912b99d3549afba70
An Nguyen <annt1290@gmail.com>
2022-08-02 23:25:20 +0700

    Remove unused env var

--- next.config.js
+++ next.config.js
@@ -3,9 +3,6 @@ const nextConfig = {
   reactStrictMode: true,
   swcMinify: true,
   output: 'standalone',
-  env: {
-    VERSION_HASH: process.env.VERSION_HASH,
-  },
 }
 
 module.exports = nextConfig
